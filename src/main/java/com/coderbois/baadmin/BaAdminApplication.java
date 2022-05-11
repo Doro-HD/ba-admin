a7f7f313f92063533a4e8ddb6140b2080075dbe8
david <david.bjakobsen@gmail.com>
2022-05-11 14:20:09 +0200

    init

---
+++ src/main/java/com/coderbois/baadmin/BaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.coderbois.baadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coderbois/baadmin/BaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderbois.baadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
