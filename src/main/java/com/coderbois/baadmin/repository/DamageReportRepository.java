41479e879f4e3410e692a8c7d9c8ec028b108fb0
Troels <troe1912@stud.kea.dk>
2022-05-12 11:28:45 +0200

    Tilføjet kommentare

--- src/main/java/com/coderbois/baadmin/repository/DamageReportRepository.java
+++ src/main/java/com/coderbois/baadmin/repository/DamageReportRepository.java
@@ -1,4 +1,22 @@
 package com.coderbois.baadmin.repository;
 
+import com.coderbois.baadmin.model.DamageReport;
+//Klasse oprettet af Troels.
+//Tilføjet createDamageReport, findDamageReportByCarNumber, addDamageToDamageReport
 public class DamageReportRepository {
+
+
+    public boolean createDamageReport(){
+        return false;
+    }
+
+    public DamageReport findDamageReportByCarNumber(int number){
+        DamageReport dmReport = new DamageReport();
+
+        return dmReport;
+    }
+
+    public boolean addDamageToDamageReport(){
+        return false;
+    }
 }
