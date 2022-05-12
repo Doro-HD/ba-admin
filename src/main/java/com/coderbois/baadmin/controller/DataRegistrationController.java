a22de19faec7683d3a12ba1fd8b79d63d10fcba8
Lasse-R <lass531n@stud.kea.dk>
2022-05-12 12:00:41 +0200

    Car model oprettet, getAllCars oprettet i CarService og CarRepository

--- src/main/java/com/coderbois/baadmin/controller/DataRegistrationController.java
+++ src/main/java/com/coderbois/baadmin/controller/DataRegistrationController.java
@@ -1,4 +1,27 @@
 package com.coderbois.baadmin.controller;
 
+import com.coderbois.baadmin.model.Car;
+import com.coderbois.baadmin.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+
+@Controller
 public class DataRegistrationController {
+
+      private final CarService carService;
+
+      @Autowired
+      public DataRegistrationController(CarService carService){
+            this.carService = carService;
+      }
+
+      @GetMapping("/")
+      @ResponseBody
+      public ArrayList<Car> getAllCars(){
+            return carService.getAllCars();
+      }
 }
--- src/main/java/com/coderbois/baadmin/service/CarService.java
+++ src/main/java/com/coderbois/baadmin/service/CarService.java
@@ -1,4 +1,24 @@
 package com.coderbois.baadmin.service;
 
+import com.coderbois.baadmin.model.Car;
+import com.coderbois.baadmin.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class CarService {
+
+      private final CarRepository carRepository;
+
+      @Autowired
+      public CarService(CarRepository carRepository){
+            this.carRepository = carRepository;
+      }
+
+      public ArrayList<Car> getAllCars(){
+            return carRepository.getAllCars();
+      }
+
 }
