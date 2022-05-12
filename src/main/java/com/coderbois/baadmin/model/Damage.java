a694b67a4ab5396a1d2ca7f96c4aaede4d361f1f
Troels <troe1912@stud.kea.dk>
2022-05-12 11:14:18 +0200

    initialized Damage and DamageReport models

--- src/main/java/com/coderbois/baadmin/model/Damage.java
+++ src/main/java/com/coderbois/baadmin/model/Damage.java
@@ -1,4 +1,26 @@
 package com.coderbois.baadmin.model;
 
 public class Damage {
+
+    private String damageType;
+    private int price;
+
+    public Damage() {
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDamageType() {
+        return damageType;
+    }
+
+    public void setDamageType(String damageType) {
+        this.damageType = damageType;
+    }
 }
--- src/main/java/com/coderbois/baadmin/model/DamageReport.java
+++ src/main/java/com/coderbois/baadmin/model/DamageReport.java
@@ -1,4 +1,19 @@
 package com.coderbois.baadmin.model;
 
+import java.util.List;
+
 public class DamageReport {
+
+    private List<Damage> damage;
+    private double totalCost;
+    private int carNumber;
+
+    public DamageReport(){}
+
+    public boolean addDamage(){
+
+
+        return false;
+    }
+
 }
