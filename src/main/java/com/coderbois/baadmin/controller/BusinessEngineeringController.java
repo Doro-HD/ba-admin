a4bd982572422195685dcf535f3d8d3419dc0dc2
victor-dat21b <vict702k@stud.kea.dk>
2022-05-13 12:43:07 +0200

    Oprettet JUnit metode, samt lavet resten af de funktionelle features til forretningsudviklerne

--- src/main/java/com/coderbois/baadmin/controller/BusinessEngineeringController.java
+++ src/main/java/com/coderbois/baadmin/controller/BusinessEngineeringController.java
@@ -1,18 +1,31 @@
 package com.coderbois.baadmin.controller;
 
 
+import com.coderbois.baadmin.service.LeaseService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Controller
 public class BusinessEngineeringController {
+    private final LeaseService leaseService;
+
+    public BusinessEngineeringController(LeaseService leaseService){
+        this.leaseService = leaseService;
+    }
+
 
 
     @GetMapping("/busistats")
-    public String getBusinessStatistic(){
-
+    public String getBusinessStatistic(Model model ){
+        model.addAttribute("leasestatistic", this.leaseService.calculateBusinessInfo());
         return "businessStats";
     }
+
 
 
 }
