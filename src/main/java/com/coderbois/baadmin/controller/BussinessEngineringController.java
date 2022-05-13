74be6a0976dc94d74e5f6a92d5c54db0a90c4701
victor-dat21b <vict702k@stud.kea.dk>
2022-05-13 10:46:02 +0200

    Controller and LeasingStatistic filled in

--- src/main/java/com/coderbois/baadmin/controller/BussinessEngineringController.java
+++ src/main/java/com/coderbois/baadmin/controller/BussinessEngineringController.java
@@ -1,4 +1,18 @@
 package com.coderbois.baadmin.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BussinessEngineringController {
+
+
+    @GetMapping("/busstats")
+    public String getBusinessStatistic(){
+
+        return "businessstats";
+    }
+
+
 }
--- src/main/java/com/coderbois/baadmin/service/LeasingStatistic.java
+++ src/main/java/com/coderbois/baadmin/service/LeasingStatistic.java
@@ -1,4 +1,51 @@
 package com.coderbois.baadmin.service;
 
+import com.coderbois.baadmin.model.Lease;
+
+import java.util.ArrayList;
+
 public class LeasingStatistic {
+
+
+    private ArrayList<Lease> leases;
+    private double totalPayout;
+    private int totalLeases;
+
+    public LeasingStatistic(ArrayList<Lease> leases, double totalPayout, int totalLeases){
+        this.leases = leases;
+        this.totalPayout = totalPayout;
+        this.totalLeases = totalLeases;
+    }
+
+
+
+
+    public ArrayList<Lease> getLeases() {
+        return leases;
+    }
+
+    public double getTotalPayout() {
+        return totalPayout;
+    }
+
+    public int getTotalLeases() {
+        return totalLeases;
+    }
+
+
+    public void setLeases(ArrayList<Lease> leases) {
+        this.leases = leases;
+    }
+
+    public void setTotalPayout(double totalPayout) {
+        this.totalPayout = totalPayout;
+    }
+
+    public void setTotalLeases(int totalLeases) {
+        this.totalLeases = totalLeases;
+    }
+
+
+
+
 }
