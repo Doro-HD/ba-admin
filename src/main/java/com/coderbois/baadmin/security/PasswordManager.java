47b35096c96e40deb3c02e469f4a6682273c1a15
david <david.bjakobsen@gmail.com>
2022-05-13 12:09:48 +0200

    init loginFeature

--- src/main/java/com/coderbois/baadmin/security/PasswordManager.java
+++ src/main/java/com/coderbois/baadmin/security/PasswordManager.java
@@ -1,15 +1,15 @@
 package com.coderbois.baadmin.security;
 
-//Klasse taget fra miniprojekt: oenskebroenen
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+//Klasse og metoder kopieret fra miniprojekt: oenskebroenen
 public class PasswordManager {
-    /*
     public String createHashedPassword(String password) {
-        String salt = BCrypt.gensalt(10);
+        String salt = BCrypt.gensalt(12);
         return BCrypt.hashpw(password, salt);
     }
 
     public boolean validatePassword(String password, String hashedPassword) {
         return BCrypt.checkpw(password, hashedPassword);
     }
-     */
 }
