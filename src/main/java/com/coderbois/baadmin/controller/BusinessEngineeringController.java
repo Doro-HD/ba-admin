2fc1d9e30e26b5b09954a1cbad50aec5310d44ec
DoroHD <david.bjakobsen@gmail.com>
2022-05-18 12:43:57 +0200

    Removed duplicate dependency in pom.xml

--- src/main/java/com/coderbois/baadmin/controller/BusinessEngineeringController.java
+++ src/main/java/com/coderbois/baadmin/controller/BusinessEngineeringController.java
@@ -2,13 +2,9 @@ package com.coderbois.baadmin.controller;
 
 
 import com.coderbois.baadmin.service.LeaseService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Controller
 public class BusinessEngineeringController {
@@ -18,14 +14,9 @@ public class BusinessEngineeringController {
         this.leaseService = leaseService;
     }
 
-
-
     @GetMapping("/busistats")
     public String getBusinessStatistic(Model model ){
         model.addAttribute("leasestatistic", this.leaseService.calculateBusinessInfo());
         return "businessStats";
     }
-
-
-
 }
