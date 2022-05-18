047f711cfc353e188b8ae054f51b39a768b174cf
victor-dat21b <vict702k@stud.kea.dk>
2022-05-18 12:51:43 +0200

    Moved test

---
+++ src/test/java/com/coderbois/baadmin/service/BaAdminServiceTests.java
@@ -0,0 +1,24 @@
+package com.coderbois.baadmin.service;
+
+import com.coderbois.baadmin.model.Lease;
+import com.coderbois.baadmin.repository.LeaseRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BaAdminServiceTests {
+
+
+    @Test
+    public void calculateBusinessInformationBasedOnLeases(){
+        ArrayList<Lease> allLeases = new LeaseRepository().getAllLeases();
+        double allAmount = 0;
+        for (Lease lease: allLeases){
+            allAmount = allAmount + lease.getMonthlyPay();
+        }
+        int countLease = allLeases.size();
+        assertNotNull(new LeasingStatistic(allLeases, allAmount, countLease));
+    }
+}
